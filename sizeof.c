585fe085aabfd04ee82189620fe0386d11e4abdc
BillSkiCO <BillGolembieski@projectu23.com>
2015-07-21 02:50:08 -0400

    get size in bytes of variables

---
+++ sizeof.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    char b;
+    double c;
+    float d;
+    long long int e;
+    signed int f;
+    long int g;
+    unsigned long long int h;
+    
+    printf("int: %i\n", sizeof(a));
+    printf("char: %i\n", sizeof(b));
+    printf("double: %i\n", sizeof(c));
+    printf("float: %i\n", sizeof(d));
+    printf("long long int: %i\n", sizeof(e));
+    printf("signed int: %i\n", sizeof(f));
+    printf("long int: %i\n", sizeof(g));
+    printf("unsigned long long int: %i\n", sizeof(h));
+
+
+}
