c8cb49c2e8945fbca62f000f51ecd84b33f4ef92
Golembieski <BillGolembieski@projectu23.com>
2015-11-24 11:16:48 -0500

    Update mario.c

--- mario.c
+++ mario.c
@@ -2,34 +2,55 @@
 
 // Function Prototypes
 void insertSpace(int current_row);
-void insertHash(int current_row);
-int i,j,k,n;
+void insertSymbol(int current_row);
+void printHalfPyramid(int hieght);
+int i,j,k,user_input;
+int top_step_width = 2;
+int retry;
 
-int main(void) {
+int main(void) 
+{
+do
+{
+printf("Height: ");
+scanf("%i", user_input);
 
-//Constructors
-
-n = 8;
-for(i = 0; i < n; i++)                  // i = current row being printed
-{                                       // top down.
-    insertSpace(i);
-    insertHash(i);
-    printf("\n");
+if (user_input < 1)
+    retry == 1;
+else
+    retry == 0;
 }
+while(retry == 1);
+
+printHalfPyramid(user_input);
 return 0;
 }
 
+void printHalfPyramid(int hieght)
+{
+    for(i = 0; i < hieght; i++)                     // i = current row being printed
+{                                                   // top down.
+    insertSpace(i);
+    insertSymbol(i);
+    printf("\n");
+}
+}
 void insertSpace(int current_row)
 {
-    for(j=0; j < (n - (current_row + 1)); j++)
+    // Inserts the correct number of spaces to right align.
+    // This function not needed if left aligning #'s.
+    
+    for(j = 0; j < (user_input - (current_row + 1)); j++)  // Height - (Row + 1)
     {
         printf(" ");
     }
 }
 
-void insertHash(int current_row)
+void insertSymbol(int current_row)
 {
-    for(k=0; k < (current_row + 2); k++)
+    // Inserts the correct number of #'s starting with
+    // the first row having 2 #'s, then 
+    for(k = 0; k < (current_row + top_step_width); k++)
     {
         printf("#");
     }
